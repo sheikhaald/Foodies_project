4d2ac2c0be1ed8eb91e15e161cc820259deb7e28
sheikhaald <smaldeai98@gmail.com>
2023-11-08 20:01:39 +0300

    pus

--- api/Ingredient/ingredient.routes.js
+++ api/Ingredient/ingredient.routes.js
@@ -4,6 +4,7 @@ const {
   getALLingredient,
   getOneingredient,
   findIngredient,
+  addIngredientToRecipe,
 } = require("./ingredient.controllers");
 const router = express.Router();
 
@@ -12,8 +13,14 @@ router.param("ingID", async (req, res, next, ingID) => {
   req.ingredient = ingredient;
   next();
 });
+router.param("RecipeId", async (req, res, next, RecipeId) => {
+  const recipe = await findRecipe(RecipeId, next);
+  req.recipe = recipe;
+  next();
+});
 router.post("/", createIngredient);
 router.get("/", getALLingredient);
 router.get("/:ingID", getOneingredient);
+router.put("/:ingID/:RecipeId", addIngredientToRecipe);
 
 module.exports = router;
